4b6a90d0197b302cbb59a87ae0603f938f728281
nulloneguy <87767572+nulloneguy@users.noreply.github.com>
2021-11-17 22:01:56 +0700

    add Montserrat font

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,10 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700,900&display=swap");
+
 body {
+  font-family: "Montserrat", sans-serif;
   background-color: #1B2B34;
   padding: 0;
   max-width: none;
